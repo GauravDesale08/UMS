f13fed5ec588bcd08eb4306b28d18efddd498741
GauravDesale08 <gauravdesale2020@gmail.com>
2024-09-12 22:45:21 +0530

    ums

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,36 @@
+import 'dart:ui';
+
+class AppColors {
+  // Light theme colors
+  static const bgLight = Color(0xFFF4F4F4);
+  static const bgSecondayLight = Color(0xFFFCFCFC);
+  static const titleLight = Color(0xFF272B30);
+  static const textLight = Color(0xFF6F767E);
+  static const textGrey = Color(0xFF9A9FA5);
+  static const iconLight = Color(0xFF272B30);
+  static const iconBlack = Color(0xFF1A1D1F);
+  static const iconGrey = Color(0xFF9A9FA5);
+  static const highlightLight = Color(0xFFEFEFEF);
+
+  static const primary = Color(0xFF2A85FF);
+  static const primaryPurple = Color(0xFF8E59FF);
+  static const success = Color(0xFF83BF6E);
+  static const error = Color(0xFFFF6A55);
+
+  static const secondaryMistyrose = Color(0xFFFFE7E4);
+  static const secondaryPeach = Color(0xFFFFBC99);
+  static const secondaryLavender = Color(0xFFCABDFF);
+  static const secondaryBabyBlue = Color(0xFFB1E5FC);
+  static const secondaryMintGreen = Color(0xFFB5E4CA);
+  static const secondaryPaleYellow = Color(0xFFFFD88D);
+
+  // Dark theme colors
+  static const bgDark = Color(0xFF1A1A1A);
+  static const bgSecondayDark = Color(0xFF2C2C2C);
+  static const titleDark = Color(0xFFE1E1E1);
+  static const textDark = Color(0xFFB3B3B3);
+  static const textGreyDark = Color(0xFF7D7D7D);
+  static const iconDark = Color(0xFFE1E1E1);
+  static const iconGreyDark = Color(0xFF7D7D7D);
+  static const highlightDark = Color(0xFF444444);
+}
